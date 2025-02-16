fda5ade20e5231b5e02773e89ef081680f2b3ad2
Yuvraj Sharma <yuvrajsharma9109@gmail.com>
2025-02-16 15:20:06 +0530

    max of pcount or ncount

---
+++ count_max_neg_pos.c
@@ -0,0 +1,18 @@
+int maximumCount(int* nums, int numsSize) {
+  int pcount=0;
+  int ncount=0;
+  for (int i=0;i<numsSize;i++){
+      
+      if(nums[i]<0){
+          pcount++;
+      }
+      else if (nums[i]>0) {
+          ncount++;
+      }
+      else{
+          continue;
+      } 
+  }
+  
+  return fmax(ncount,pcount);
+}
